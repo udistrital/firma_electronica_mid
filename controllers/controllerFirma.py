472ddbb26d4c98b3ef4ce5eb39611fc0ee94b065
Skyrus1305 <dagorin7@gmail.com>
2024-05-30 20:56:45 -0500

    Se realiza integración de la API firma electrónica con nueva funcionalidad de gestor Documental para subir a Nuxeo el documento firmado

--- controllers/controllerFirma.py
+++ controllers/controllerFirma.py
@@ -3,9 +3,9 @@ from flask import Flask,jsonify,request, Response
 from models.firma import firmar
 from models.firma_electronica import ElectronicSign
 
-def postFirmaElectronica(body):
+def postFirmaElectronica(data):
+    response_array=[]
     try:
-        data=body
         for i in range(len(data)):
             IdDocumento = data[i]['IdTipoDocumento']
             res = requests.get(str(os.environ['DOCUMENTOS_CRUD_URL'])+'/tipo_documento/'+str(IdDocumento))
@@ -14,16 +14,14 @@ def postFirmaElectronica(body):
                 return Response(json.dumps({'Status':'404','Error': str("the id "+str(data[i]['IdTipoDocumento'])+" does not exist in documents_crud")}), status=404, mimetype='application/json')
 
             res_json = json.loads(res.content.decode('utf8').replace("'", '"'))
-
             blob = base64.b64decode(data[i]['file'])
             with open(os.path.expanduser('./documents/documentToSign.pdf'), 'wb') as fout:
                 fout.write(blob)
-
             jsonFirmantes = {
                     "firmantes": data[i]["firmantes"],
                     "representantes": data[i]["representantes"],
             }
-
+            all_metadata = str({** data[i]['metadatos']}).replace("{'", '{\\"').replace("': '", '\\":\\"').replace("': ", '\\":').replace(", '", ',\\"').replace("',", '",').replace('",' , '\\",').replace("'}", '\\"}').replace('\\"', '\"')
             DicPostDoc = {
                 'Metadatos': all_metadata,
                 'Nombre': data[i]['nombre'],
@@ -33,9 +31,7 @@ def postFirmaElectronica(body):
             }
             resPost = requests.post(str(os.environ['DOCUMENTOS_CRUD_URL'])+'/documento', json=DicPostDoc).content
             responsePostDoc = json.loads(resPost.decode('utf8').replace("'", '"'))
-
             firma_electronica = firmar(str(data[i]['file']))
-
             electronicSign = ElectronicSign()
             firma_completa = electronicSign.firmaCompleta(firma_electronica["llaves"]["firma"], responsePostDoc["Id"])
             objFirmaElectronica = {
@@ -44,8 +40,9 @@ def postFirmaElectronica(body):
                 "FirmaEncriptada": firma_completa,
                 "Firmantes": json.dumps(jsonFirmantes),
                 "Llaves": json.dumps(firma_electronica["llaves"]),
+                "DocumentoId": {"Id": responsePostDoc["Id"]},
             }
-            reqPostFirma = requests.post(str(os.environ['DOCUMENTOS_CRUD_URL'])+'/firma_electronica', json=objFirmaElectronica).content
+            reqPostFirma = requests.post(str(os.environ['DOCUMENTOS_CRUD_URL'])+'firma_electronica', json=objFirmaElectronica).content
             responsePostFirma = json.loads(reqPostFirma.decode('utf8').replace("'", '"'))
             datos = {
                 "firma": responsePostFirma["Id"],
@@ -53,7 +50,6 @@ def postFirmaElectronica(body):
                 "representantes": data[i]["representantes"],
                 "tipo_documento": res_json["Nombre"],
             }
-
             electronicSign.estamparFirmaElectronica(datos)
             jsonStringFirmantes = {
                 "firmantes": json.dumps(data[i]["firmantes"]),
@@ -61,7 +57,6 @@ def postFirmaElectronica(body):
             }
 
             all_metadata = str({** firma_electronica, ** data[i]['metadatos'],  ** jsonStringFirmantes}).replace("{'", '{\\"').replace("': '", '\\":\\"').replace("': ", '\\":').replace(", '", ',\\"').replace("',", '",').replace('",' , '\\",').replace("'}", '\\"}').replace('\\"', '\"').replace("[", "").replace("]", "").replace('"{', '{').replace('}"', '}').replace(": ", ":").replace(", ", ",").replace("[", "").replace("]", "").replace("},{", ",")
-
             DicPostDoc = {
                 'Metadatos': all_metadata,
                 "firmantes": data[i]["firmantes"],
@@ -72,14 +67,35 @@ def postFirmaElectronica(body):
                 'Activo': True
             }
 
-            putUpdateJson = {
+            putUpdateJson = [{
                 "IdTipoDocumento": data[i]['IdTipoDocumento'],
                 "nombre": data[i]['nombre'],
                 "metadatos": all_metadata,
                 "descripcion": data[i]['descripcion'],
-                "file": data[i]['file'],
+                "file": str(electronicSign.docFirmadoBase64()),
                 "idDocumento": responsePostDoc["Id"]
-            }
+            }]
+            reqPutFirma = requests.put(str(os.environ['GESTOR_DOCUMENTAL_URL'])+'document/putUpdate', json=putUpdateJson).content
+            responsePutUpdate = json.loads(reqPutFirma.decode('utf8').replace("'", '"'))
+            response_array.append(responsePutUpdate)
+        responsePutUpdate = response_array if len(response_array) > 1 else responsePutUpdate
+        return Response(json.dumps({'Status':'200', 'res':responsePutUpdate}), status=200, mimetype='application/json')
     except Exception as e:
-        logging.error("type error: "+str(e))
-    
+        logging.error("type error: " + str(e))
+
+        if str(e) == "'IdTipoDocumento'":
+            error_dict = {'Status':'the field IdTipoDocumento is required','Code':'400'}
+            return Response(json.dumps(error_dict), status=400, mimetype='application/json')
+        elif str(e) == "'nombre'":
+            error_dict = {'Status':'the field nombre is required','Code':'400'}
+            return Response(json.dumps(error_dict), status=400, mimetype='application/json')
+        elif str(e) == "'file'":
+            error_dict = {'Status':'the field file is required','Code':'400'}
+            return Response(json.dumps(error_dict), status=400, mimetype='application/json')
+        elif str(e) == "'metadatos'":
+            error_dict = {'Status':'the field metadatos is required','Code':'400'}
+            return Response(json.dumps(error_dict), status=400, mimetype='application/json')
+        elif '400' in str(e):
+            DicStatus = {'Status':'invalid request body', 'Code':'400'}
+            return Response(json.dumps(DicStatus), status=400, mimetype='application/json')
+        return Response(json.dumps({'Status':'500','Error':str(e)}), status=500, mimetype='application/json')
