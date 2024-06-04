d1fb054f57a6ed7ee48e06c7c3666043128dc149
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-06-04 15:11:40 -0500

    fix: Ajuste para despliegue

--- api.py
+++ api.py
@@ -3,9 +3,11 @@ from flask import Flask, jsonify, request, send_from_directory
 from conf import conf
 from controllers import error
 from routers import router
+conf.checkEnv()
 
 app = Flask(__name__) #Creo la app de servidor
-error.add_error_handler(app)
+
 router.addRutas(app)
+error.add_error_handler(app)
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=int(os.environ['API_PORT']))
